49cab25a775e3de573e06168e0ae7420dcdcd5c3
RomeiroM <romeiromaria9@gmail.com>
2023-12-11 15:56:07 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/divulgapet/VisualizaPet.kt
@@ -0,0 +1,11 @@
+package com.example.divulgapet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VisualizaPet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visualiza_pet)
+    }
+}
