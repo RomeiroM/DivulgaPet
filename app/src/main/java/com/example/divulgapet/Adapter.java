7d238e2e0cd66fd6ece69789877502fd4bbc3611
RomeiroM <romeiromaria9@gmail.com>
2023-12-12 22:31:26 -0300

    Implementação menu navegação

---
+++ app/src/main/java/com/example/divulgapet/Adapter.java
@@ -0,0 +1,5 @@
+package com.example.divulgapet;
+
+public class Adapter {
+
+}
--- app/src/main/java/com/example/divulgapet/MainActivity.kt
+++ app/src/main/java/com/example/divulgapet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.divulgapet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            supportActionBar?.hide()
+
+            Handler(Looper.getMainLooper()).postDelayed({},5000)
+
+        }
     }
-}
